9277fedc741109eca83ee434a7a4f362da3a6bb9
MarioAlves41 <marioab641@gmail.com>
2022-11-29 13:29:05 -0300

    primeiro commit

---
+++ gatsby-config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  siteMetadata: {
+    title: `Gatsby Default Starter`,
+  },
+  plugins: [
+    {
+      resolve: 'gatsby-source-contentful',
+      options: {
+        spaceId: process.env.CF_SPACE || '3bc97k4uk5q7',
+        accessToken: process.env.CF_TOKEN || 'cad8217f92693dac2350540d49d51dd459a8c9a9c2729dcc90409a96e197cb48'
+      }
+    },
+    `gatsby-transformer-remark`,
+    `gatsby-plugin-react-helmet`
+  ],
+}
